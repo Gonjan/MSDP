7677d80743fc47cd02fc1a0b486f56c31f3c1918
Gonjan <lgjcqupt@sina.com>
2018-01-09 17:30:42 +0800

    add insertByBatch method

---
+++ src/main/java/dataProcess/service/impl/WeightAdult2DGroupServiceImpl.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package dataProcess.service.impl;
+
+/**
+ *
+ * @author Gonjan
+ * @version $Id: WeightAdult2DGroupServiceImpl.java, v 0.1 2018年01月09日 14:57 Gonjan Exp $
+ */
+public class WeightAdult2DGroupServiceImpl {
+}
---
+++ src/main/java/dataProcess/service/impl/WeightAdult3DGroupServiceImpl.java
@@ -0,0 +1,13 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package dataProcess.service.impl;
+
+/**
+ *
+ * @author Gonjan
+ * @version $Id: WeightAdult3DGroupServiceImpl.java, v 0.1 2018年01月09日 14:58 Gonjan Exp $
+ */
+public class WeightAdult3DGroupServiceImpl {
+}
